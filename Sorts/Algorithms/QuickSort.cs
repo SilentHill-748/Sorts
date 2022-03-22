d7a552396932f0070669cb0639e54f9d1ce53548
SilentHill <Nikich213@gmail.com>
2022-03-22 10:24:14 +0500

    Optimization of quick sort.

--- Sorts/Algorithms/QuickSort.cs
+++ Sorts/Algorithms/QuickSort.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sorts.Algorithms
 {
@@ -14,71 +11,41 @@ namespace Sorts.Algorithms
 
         public override void Sort()
         {
-            SortedCollection = Sort(_list);
+            Sort(0, _list.Count - 1);
+            SortedCollection = _list;
         }
 
-        public List<T> Sort(List<T> list)
+        private void Sort(int left, int right)
         {
-            if (list.Count < 2)
-                return list;
+            if (left >= right) return;
 
+            int pivot = Partition(left, right);
+            Sort(left, pivot);
+            Sort(pivot + 1, right);
+        }
+
+        private int Partition(int left, int right)
+        {
             Random rnd = new();
-            int pivotIndex = rnd.Next(list.Count);
+            T pivotVal = _list[rnd.Next(left, right + 1)];
 
-            List<T> left = new();
-            List<T> right = new();
-
-            for (int i = 0; i < list.Count; i++)
+            while (left < right)
             {
-                if (i == pivotIndex)
-                    continue;
+                while (_list[left].CompareTo(pivotVal) == -1)
+                    left++;
 
-                if (list[i].CompareTo(list[pivotIndex]) <= 0)
-                    left.Add(list[i]);
-                else
-                    right.Add(list[i]);
+                while (_list[right].CompareTo(pivotVal) == 1)
+                    right--;
+
+                if (left < right)
+                    (_list[right], _list[left]) = (_list[left], _list[right]);
+
+                // Ignore duplicates
+                if (_list[left].CompareTo(_list[right]) == 0)
+                    right--;
             }
 
-            var sortedLeft = Sort(left);
-            var sortedRight = Sort(right);
-
-            sortedLeft.Add(list[pivotIndex]);
-            sortedLeft.AddRange(sortedRight);
-
-            return sortedLeft;
+            return left;
         }
-
-
-        // Dosn't work in collections, which contains duplicates.
-
-        //private void Sort(List<T> collection, int left, int right)
-        //{
-        //    if (left < right)
-        //    {
-        //        int partition = Partition(collection);
-        //        Sort(collection, 0, partition);
-        //        Sort(collection, partition + 1, right);
-        //    }
-        //}
-
-        //private static int Partition(List<T> list)
-        //{
-        //    int left = 0;
-        //    int right = list.Count - 1;
-        //    int pivot = new Random().Next(right + 1);
-
-        //    while (left < right)
-        //    {
-        //        while (list[left].CompareTo(list[pivot]) == -1)
-        //            left++;
-
-        //        while (list[right].CompareTo(list[pivot]) == 1)
-        //            right--;
-
-        //        (list[right], list[left]) = (list[left], list[right]);
-        //    }
-
-        //    return right;
-        //}
     }
 }
